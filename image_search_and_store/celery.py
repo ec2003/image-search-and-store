f065f9d830b89a33e515e9b2f55ad6e26a132062
ec2003 <hieu040390@gmail.com>
2026-06-14 20:34:57 +0700

    Commit restart project

--- image_search_and_store/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "image_search_and_store.settings")
-
-app = Celery("image_search_and_store")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
